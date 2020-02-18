69015751a9ed449bd7c57298ca0d5e5f4686f866
Nicola Brisotto <nicolabrisotto@gmail.com>
2020-02-18 09:23:39 +0100

    arguments will print also the properties of a function object

--- node-modules-under-the-hood/arguments_example.js
+++ node-modules-under-the-hood/arguments_example.js
@@ -7,5 +7,10 @@ example(1,2)
 function myfunction(param1, param2) {
     
 }
+
+
 example(myfunction, "ciao")
 
+//arguments will print also the properties of a function object
+myfunction.casa=2
+example(myfunction, "ciao")
